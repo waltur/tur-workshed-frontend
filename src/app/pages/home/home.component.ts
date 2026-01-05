5fd85217660c1d29d8a7f23e09771bda6826ed2e
waltur <tur-87@hotmail.com>
2026-01-04 22:05:22 -0500

    frontal

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+ngAfterViewInit() {
+  const observer = new IntersectionObserver(
+    entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('show');
+        }
+      });
+    },
+    { threshold: 0.2 }
+  );
+
+  document
+    .querySelectorAll('.animate-section')
+    .forEach(el => observer.observe(el));
+}
 }
