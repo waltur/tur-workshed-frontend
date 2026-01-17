b81da1cffa7f5ba6550d836d96a1b538f0758085
waltur <tur-87@hotmail.com>
2026-01-17 18:05:59 -0500

    delete groups

--- src/app/modules/groups/pages/group-list/group-list.component.ts
+++ src/app/modules/groups/pages/group-list/group-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { GroupService } from '../../services/group.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-group-list',
@@ -14,5 +15,32 @@ export class GroupListComponent implements OnInit {
     console.log("grupo list");
     this.groupService.getGroups().subscribe(data => this.groups = data);
        console.log("this.groups",this.groups);
+  }
+ onDeleteGroup(id: number) {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: 'This group will be deleted permanently',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, delete it',
+      cancelButtonText: 'Cancel'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.groupService.deleteGroup(id).subscribe({
+          next: () => {
+            Swal.fire('Deleted!', 'The group has been deleted.', 'success');
+            this.loadGroups(); // vuelve a cargar la lista
+          },
+          error: () => {
+            Swal.fire('Error', 'Could not delete the group', 'error');
+          }
+        });
+      }
+    });
+  }
+ loadGroups() {
+    // tu método para recargar la tabla
+    this.groupService.getGroups().subscribe(data => this.groups = data);
+           console.log("this.groups",this.groups);
   }
 }
